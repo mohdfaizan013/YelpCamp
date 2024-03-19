ec1f598374fabe0acaa7ba49e879dcf9ad94d04e
MD FAIZAN <faizan96596@gmail.com>
2024-03-20 01:54:02 +0530

    changes to app and package

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 
-const dbURL = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbURL = process.env.DB_URL;
 
 //'mongodb://127.0.0.1:27017/yelp-camp'
 
